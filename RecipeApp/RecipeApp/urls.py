c9da7294479891209b94e5233858298604d01525
msegan99 <msegan@buffalo.edu>
2022-02-14 11:33:00 -0500

    Basic hello world website added

--- RecipeApp/RecipeApp/urls.py
+++ RecipeApp/RecipeApp/urls.py
@@ -1,4 +1,4 @@
-"""RecipeApp URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.hi)
 ]
