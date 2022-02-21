02fddb9be8ba53c23cbfb34f75d619725f9188c6
msegan99 <msegan@buffalo.edu>
2022-02-21 00:08:13 -0500

    Added mongo database and accounts collection

--- RecipeApp/RecipeApp/views.py
+++ RecipeApp/RecipeApp/views.py
@@ -3,7 +3,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from mysqlx import Auth
+from pyparsing import unicode_string
 from RecipeApp import settings
+from pymongo import MongoClient
+import bcrypt
+
+#Connect to mongo database here
+client = MongoClient('localhost')
+db=client['recipeApp']
+accountCollection=db['accounts']
+
 
 def home(request):
     #non logged in users will be sent the response here
@@ -21,10 +30,17 @@ def register_view(request):
         form=UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
-            #LOG IN THE USER HERE
-            user=form.get_user()
-            login(request, user)
-            return redirect('/')
+            #Add the new account to mongo database
+            username=request.POST["username"]
+            unhashedPassword=request.POST["password1"]
+            #hash password
+            salt=bcrypt.gensalt()
+            hashedPassword=bcrypt.hashpw(unhashedPassword.encode(), salt)
+            accountDoc={"username": username,
+                        "password": hashedPassword}
+            accountCollection.insert_one(accountDoc)
+            #SEND USER TO LOGIN PAGE
+            return redirect('/login')
 
     return render(request, 'auth pages/register.html', {'form': form})
 
