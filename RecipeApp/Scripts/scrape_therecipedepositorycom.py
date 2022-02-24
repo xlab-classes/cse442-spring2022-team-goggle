3b2515e04e8bd38bcd06a4e991d8eaff87efa233
Johnathan Chugh <jlchugh@buffalo.edu>
2022-02-23 20:47:58 -0500

    linking to issue

--- RecipeApp/Scripts/scrape_therecipedepositorycom.py
+++ RecipeApp/Scripts/scrape_therecipedepositorycom.py
@@ -11,6 +11,24 @@ soup = BeautifulSoup(sitemap_response.content, "html.parser")
 category_urls = soup.findAll("url")
 category_urls = [category_url.find("loc").string for category_url in category_urls]
 
+recipe_urls = []
 for category_url in category_urls:
-    print("processing \"" + url + "\"")
+    new_links_in_category = []
+    print("processing \"" + category_url + "\"")
+    # get request to the page
     page = requests.get(category_url)
+
+    # load it into BeautifulSoup
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # select the recipe item elements
+    recipes = soup.select("[class~=recipe-item]")
+
+    # collect the link in the
+    for recipe_link in recipes:
+        new_links_in_category = recipe_link.get('href')
+
+    # add the links to the collection of links
+    recipe_urls = recipe_urls + new_links_in_category
+
+    print(new_links_in_category)
