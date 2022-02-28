f0431ebeca829b3d65db22257291921b428d9fa3
= <=>
2022-02-28 15:38:07 -0500

    now the url paths are being collected

--- RecipeApp/Scripts/scrape_therecipedepositorycom.py
+++ RecipeApp/Scripts/scrape_therecipedepositorycom.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import requests
+import pdb
 
 sitemap_xml ="https://www.therecipedepository.com/site-map/categories"
 sitemap_response = requests.get(sitemap_xml)
@@ -13,22 +14,23 @@ category_urls = [category_url.find("loc").string for category_url in category_ur
 
 recipe_urls = []
 for category_url in category_urls:
-    new_links_in_category = []
     print("processing \"" + category_url + "\"")
     # get request to the page
     page = requests.get(category_url)
 
     # load it into BeautifulSoup
     soup = BeautifulSoup(page.content, "html.parser")
+    recipes_from_id = soup.find(id='recipes')
+    recipe_items = recipes_from_id.find_all(class_='recipe-item')
 
-    # select the recipe item elements
-    recipes = soup.select("[class~=recipe-item]")
+    # for each recipe html chunk: for each a: a['href'] is the suffix
+    for item in recipe_items:
+        for a in item.find_all('a'):
+            print(a['href'])
 
-    # collect the link in the
-    for recipe_link in recipes:
-        new_links_in_category = recipe_link.get('href')
+
 
     # add the links to the collection of links
-    recipe_urls = recipe_urls + new_links_in_category
+    #recipe_urls = recipe_urls + new_links_in_category
 
-    print(new_links_in_category)
+    #print(new_links_in_category)
