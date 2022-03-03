dd44ced663baedd7a29fc9e6b6c22e73c6083396
MohamadDibassy <mbdibass@buffalo.edu>
2022-03-03 16:05:41 -0500

    Add functions from index to views

--- RecipeApp/RecipeApp/urls.py
+++ RecipeApp/RecipeApp/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from . import index
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #path('', views.hi)
-    path('',index.webpage1, name='webpage1'),
-    path('new-recipe',index.webpage4,name='webpage4'),
-    path('recipe',index.webpage3,name='webpage3'),
-    path('register',index.webpage2,name='webpage2'),
-    path('saved-recipes',index.webpage5,name='webpage5'),
-    path('search-result',index.webpage6,name='webpage6'),
+    path('/', views.webpage1,name='webpage1'),
+    path('new-recipe/', views.webpage4,name='webpage2'),
+    path('recipe/', views.webpage3,name='webpage3'),
+    path('register/', views.webpage2,name='webpage4'),
+    path('saved-recipes/', views.webpage5,name='webpage5'),
+    path('search-result/', views.webpage6,name='webpage6'),
 ]
