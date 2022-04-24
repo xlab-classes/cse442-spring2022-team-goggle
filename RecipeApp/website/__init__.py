cec7021a7292b5e1a3516a866d3de4225aed4cfa
= <=>
2022-04-23 22:28:56 -0400

    started working on creating the databases

--- RecipeApp/website/__init__.py
+++ RecipeApp/website/__init__.py
@@ -1,24 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "cse442_2022_spring_team_e_db"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'jflsiejwlkjsdf'
-    app.config['SQLALCHEMY_DATABASE_URI'] = mysql://jlchugh:50335580@oceanus.cse.buffalo.edu/db
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://jlchugh:50335580@oceanus.cse.buffalo.edu/db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
 
     # import the views
     from .views import views
     from .auth import auth
 
-    #register the views
+    # register the views
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Ingredient, Recipe
+
+    create_database(app)
 
 
     return app
+
+def create_database(app):
+    if not path.exists('website/'+ DB_NAME):
+        db.create_all(app=app)
+        print("database created.")
